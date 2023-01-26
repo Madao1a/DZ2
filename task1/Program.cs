2ae23bf03430fc1092ec334ef782c8cb4feda33f
MARS <leprcoin@gmail.com>
2023-01-26 23:00:15 +0400

    dz2

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+По двум заданным числам проверять является ли одно квадратом другого
+Задать номер четверти, показать диапазоны для возможных координат
+Найти расстояние между точками в пространстве 2D/3D
+int number = 6;
+if (number == 1) Console.WriteLine ("не выходной");
+if (number == 2) Console.WriteLine ("не выходной");
+if (number == 3) Console.WriteLine ("не выходной");
+if (number == 4) Console.WriteLine ("не выходной");
+if (number == 5) Console.WriteLine ("не выходной");
+if (number == 6) Console.WriteLine ("выходной");
+if (number == 7) Console.WriteLine ("выходной");*/
+
+Console.WriteLine ("Введите число ");
+int number = int.Parse (Console.ReadLine());
+while (true)
+{
+    
+}
---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine ("Введите число1 ");
+int number1 = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите число2 ");
+int number2 = int.Parse (Console.ReadLine());
+if (number1 * number1 == number2) Console.WriteLine ("Число1 являетя квадратом Числа2");
+if (number2 * number2 == number1) Console.WriteLine ("Число2 являетя квадратом Числа1");
+if (number1 * number1 != number2 && number2 * number2 != number1) Console.WriteLine ("Числа не являются квадратами друг друга");
