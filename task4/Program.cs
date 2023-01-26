744705c38cb428b79d06699a9aecae337cd73985
MARS <leprcoin@gmail.com>
2023-01-26 23:59:39 +0400

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Найти расстояние между точками в пространстве 2D/3D
+Console.WriteLine ("Введите координаты точки A x1= ");
+int x1 = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите координаты точки A y1= ");
+int y1 = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите координаты точки B x2= ");
+int x2 = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите координаты точки B y2= ");
+int y2 = int.Parse (Console.ReadLine());
+int D = (int)Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)) ;
+Console.WriteLine (D);
